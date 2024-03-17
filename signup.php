0b0170b76323f359926e0eb252f6fca581137777
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-03-17 14:13:30 +0100

    style für login und sigup

--- signup.php
+++ signup.php
@@ -1,65 +1,37 @@
 <?php
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		// something was posted
-		$user_name = $_POST['user_name'];
-		$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	// something was posted
+	$user_name = $_POST['user_name'];
+	$password = $_POST['password'];
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
-			// save to database
-			$user_id = random_num(20);
-			$query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
-			mysqli_query($con, $query);
-			header("Location: login.php");
-			die;
-		}
-		else{
-			echo "Please enter some valid information!";
-		}
+	if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+		// save to database
+		$user_id = random_num(20);
+		$query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
+		mysqli_query($con, $query);
+		header("Location: login.php");
+		die;
+	} else {
+		echo "Please enter some valid information!";
 	}
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
+	<link rel="stylesheet" href="css/style.css">
 	<title>Signup</title>
 </head>
+
 <body>
-
-	<style type="text/css">
-
-	#text{
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
-
 	<div id="box">
 		<form method="post">
 			<div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
@@ -74,4 +46,5 @@ session_start();
 	</div>
 
 </body>
+
 </html>
