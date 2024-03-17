219f2472850d610e38652ff1968be28d516148d4
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-03-17 14:49:37 +0100

    imag style

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
 session_start();
-	
-	include("connection.php");
+
+include("connection.php");
+include("functions.php");
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<link rel="stylesheet" href="css/style.css">
 	<title>My website</title>
 </head>
+
 <body>
 
 	<a href="logout.php">Logout</a>
 	<h1>This is the index page</h1>
+	<img src="pics/InProgress.jpg" alt="Image not found">
 
 	<br>
 	Hello, <?php echo $user_data['user_name']; ?>
 </body>
+
 </html>
