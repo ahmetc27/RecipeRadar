77bb965a6addadf22efa2e099ef6d80ec481f2a6
nicholaschopper <162157247+nicholaschopper@users.noreply.github.com>
2024-04-21 20:27:32 +0200

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,19 +1,3 @@
-<?php
-
-if(!isset($_SESSION['username'])){
-    $status = "ACCOUNT";                            
-} else {
-    $status = $_SESSION['username'];
-}
-
-if(!isset($_SESSION['role'])){
-    $role = 'anon';                            
-} else {
-    $role = $_SESSION['role'];
-}
-
-?>
-
 <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
     
         <a class="navbar-brand" href="index.php">
@@ -32,11 +16,25 @@ if(!isset($_SESSION['role'])){
                 <li class="nav-item">
                     <a class="nav-link" href="feed.php">HOME</a>
                 </li>
+
+                <!--
                 <li class="nav-item">
                     <a class="nav-link" href="profile.php">PROFILE</a>
                 </li>
+                -->
+
                 <li class="nav-item">
-                    <a class="nav-link" href="login.php">LOGIN</a>
+
+                    <?php
+
+                        if(!isset($_SESSION['currentSession'])){
+                            echo '<a class="nav-link" href="login.php">LOGIN</a>';       
+                        } else {
+                            echo '<a class="nav-link" href="profile.php">PROFILE</a>';
+                        }
+
+                    ?>
+
                 </li>
             </ul>
         </div>
