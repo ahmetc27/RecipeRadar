b0d44aaca4188558b0317e0d6952673f08c01339
nicholaschopper <162157247+nicholaschopper@users.noreply.github.com>
2024-04-21 20:14:50 +0200

    Update profile_form.php

--- profile_form.php
+++ profile_form.php
@@ -7,22 +7,22 @@ include('config/db_connect.php');
 $userData = $_SESSION['currentSession'];
 
 // Query to select user information based on username or user ID
-$query = "SELECT * FROM `users` WHERE `userName` = '{$userData['userName']}'";
-$result = mysqli_query($conn, $query);
+//$query = "SELECT * FROM `users` WHERE `userName` = '{$userData['userName']}'";
+//$result = mysqli_query($conn, $query);
 
-if ($result) {
+if ($userData) {
     // Fetch user data
-    $userDataFromDB = mysqli_fetch_assoc($result);
+    //$userDataFromDB = mysqli_fetch_assoc($result);
 
     // Output user information
     echo "<h1>User Information</h1>";
     echo "<ul>";
-    echo "<li>Salutation: " . $userDataFromDB['salutation'] . "</li>";
-    echo "<li>First Name: " . $userDataFromDB['firstName'] . "</li>";
-    echo "<li>Middle Name: " . $userDataFromDB['middleName'] . "</li>";
-    echo "<li>Last Name: " . $userDataFromDB['lastName'] . "</li>";
-    echo "<li>Email: " . $userDataFromDB['email'] . "</li>";
-    echo "<li>Birthday: " . $userDataFromDB['birthDate'] . "</li>";
+    echo "<li>Salutation: " . $userData['salutation'] . "</li>";
+    echo "<li>First Name: " . $userData['firstName'] . "</li>";
+    echo "<li>Middle Name: " . $userData['middleName'] . "</li>";
+    echo "<li>Last Name: " . $userData['lastName'] . "</li>";
+    echo "<li>Email: " . $userData['email'] . "</li>";
+    echo "<li>Birthday: " . $userData['birthDate'] . "</li>";
     // Add other fields as needed
     echo "</ul>";
 } else {
