dbf710b21a402d91e54b86da849d365607a7f116
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-04-28 00:30:42 +0200

    Update help.php

--- help.php
+++ help.php
@@ -10,7 +10,7 @@ include 'navbar.php';
     <title>FAQ MD Hotel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap css -->
-    <link rel="stylesheet" href="res/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <style>
         body {
@@ -112,7 +112,7 @@ include 'navbar.php';
     </div>
 
     <!-- Bootstrap js -->
-    <script src="res/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
         function toggleAnswer(answerId) {
             var answer = document.getElementById(answerId);
