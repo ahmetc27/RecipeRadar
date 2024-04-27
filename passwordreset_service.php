5dfb3ba6c37b91363f643f84cbe08a854072afc9
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-04-27 23:43:05 +0200

    Update passwordreset_service.php

--- passwordreset_service.php
+++ passwordreset_service.php
@@ -1,5 +1,13 @@
 <?php
-    session_start();
+session_start();
+
+if (!isset($_SESSION['currentSession'])) {
+    header("Location: login.php");
+    exit();
+}
+
+include('config/db_connect.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -19,27 +27,11 @@
         
         </header>
 
-        <main class="page-content d-lg-block py-5 align-items-center">
+        <main class="page-content d-lg-block py-5 align-items-center ">
 
-            <div class="container">
-
-                <div class="row justify-content-center">
-                        
-                    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
-
-                        <h4 class="untertitle mb-1 mt-5">Registrierung</h4>
-
-                        <hr>
-
-                        <?php
-                            include('passwordreset_form.php');
-                        ?>
-
-                    </div>
-                
-                </div>
-            
-            </div>
+            <?php
+                    include('passwordreset_form.php');
+            ?>
 
         </main>
 
@@ -48,8 +40,8 @@
         ?>
 
         <!-- Bootsttap js -->
-        <script src="res/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+         <!-- warum brauchen wir es hier? -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     </body>
 
