a298d2a521414f95ecc229923b40b28ba74ce24c
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-04-28 00:49:28 +0200

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,42 +1,46 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
-    
-        <a class="navbar-brand" href="index.php">
 
-            <img src="pics/logo-2.png" height="50px" class="d-none d-sm-block" alt="RecipeRadar">
-        
-        </a>
+    <a class="navbar-brand" href="index.php">
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <img src="pics/logo-2.png" height="50px" class="d-none d-sm-block" alt="RecipeRadar">
 
-            <ul class="navbar-nav">
-            
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">DISCOVER</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="feed.php">HOME</a>
-                </li>
+    </a>
 
-                <!--
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+        <ul class="navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">DISCOVER</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="feed.php">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="help_Service.php">FAQ</a>
+            </li>
+
+
+            <!--
                 <li class="nav-item">
                     <a class="nav-link" href="profile.php">PROFILE</a>
                 </li>
                 -->
 
-                <li class="nav-item">
+            <li class="nav-item">
 
-                    <?php
+                <?php
 
-                        if(!isset($_SESSION['currentSession'])){
-                            echo '<a class="nav-link" href="login.php">LOGIN</a>';       
-                        } else {
-                            echo '<a class="nav-link" href="profile.php">PROFILE</a>';
-                        }
+                if (!isset($_SESSION['currentSession'])) {
+                    echo '<a class="nav-link" href="login.php">LOGIN</a>';
+                } else {
+                    echo '<a class="nav-link" href="profile.php">PROFILE</a>';
+                }
 
-                    ?>
+                ?>
 
-                </li>
-            </ul>
-        </div>
+            </li>
+        </ul>
+    </div>
     </div>
 </nav>
