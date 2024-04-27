c2fe42908a1a4cf9fee37159e79db0b9db669315
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-04-28 00:37:18 +0200

    Update help_Service.php

--- help_Service.php
+++ help_Service.php
@@ -1,56 +1,42 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
 
-    <?php
-        include('head.php');
-    ?>
+<?php
+include('head.php');
+?>
 
-    <body>
+<body>
 
-        <header>
-
-            <?php
-                include('help.php');
-            ?>
-        
-        </header>
-
-        <main class="page-content d-lg-block py-5 align-items-center">
-
-            <div class="container">
-
-                <div class="row justify-content-center">
-                        
-                    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
-
-                        <h4 class="untertitle mb-1 mt-5">Registrierung</h4>
-
-                        <hr>
-
-                        <?php
-                            include('register_form.php');
-                        ?>
-
-                    </div>
-                
-                </div>
-            
-            </div>
-
-        </main>
+    <header>
 
         <?php
-            include('footer.php');
+        include('navigation.php');
         ?>
 
-        <!-- Bootsttap js -->
-        <script src="res/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </header>
 
-    </body>
+    <div class="container">
+
+        <?php
+        include('help.php');
+        ?>
+
+    </div>
+
+    </main>
+
+    <?php
+    include('footer.php');
+    ?>
+
+    <!-- Bootsttap js -->
+    <script src="res/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
 
 </html>
