0ffcbc548a8e54d81cd0bd8308d9685d13b13803
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-04-28 17:22:00 +0200

    help.php

--- help.php
+++ help.php
@@ -1,20 +1,3 @@
-<?php
-include 'navbar.php';
-?>
-
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>FAQ MD Hotel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
     <div class="container">
         <div class="faq-card">
             <h1 class="text-center">FAQs</h1>
@@ -63,9 +46,6 @@ include 'navbar.php';
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
         function toggleAnswer(answerId) {
             var answer = document.getElementById(answerId);
@@ -75,6 +55,4 @@ include 'navbar.php';
             question.classList.toggle('active');
         }
     </script>
-</body>
-
-</html>
+    
