8c1312a3fb8b00508700ec2f2bfdb15d9798ffa5
Armin Dervisefendic <armindervisefendic@gmail.com>
2024-04-30 11:08:36 +0200

    pics+impressum

---
+++ impressum.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include('head.php');
+?>
+
+<body>
+
+    <?php
+        include('navigation.php');
+    ?>
+
+    <main>
+
+        <div class="container" style="max-width: 1200px; margin: 100px auto;">
+
+            <h1 class="display-5">Impressum</h1>
+
+        </div>
+
+        <div class="container" style="max-width: 1200px; margin: 100px auto;">
+            <div class="row profile-row">
+
+                <div class='col-sm-6 col-md-3'>
+                    <div class='card'>
+                        <img class='card-img-top' src='pics/pfp/pfp1.png'>
+                        <div class='card-body' style='text-align: center;'>
+                            <p>Aissa Mohamad Iyad </p>
+                            <br>
+                            <a>Email: info@recipe-radar.at</a>
+                            <br>
+                            <a>Phone: 123-456-7890</a>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-sm-6 col-md-3'>
+                    <div class='card'>
+                        <img class='card-img-top' src='pics/pfp/pfp2.png'>
+                        <div class='card-body' style='text-align: center;'>
+                            <p>Arapcic Benjamin</p>
+                            <br>
+                            <a>Email: info@recipe-radar.at</a>
+                            <br>
+                            <a>Phone: 123-456-7890</a>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-sm-6 col-md-3'>
+                    <div class='card'>
+                        <img class='card-img-top' src='pics/pfp/pfp3.png'>
+                        <div class='card-body' style='text-align: center;'>
+                            <p>Cicek Ahmet</p>
+                            <br>
+                            <a>Email: info@recipe-radar.at</a>
+                            <br>
+                            <a>Phone: 123-456-7890</a>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-sm-6 col-md-3'>
+                    <div class='card'>
+                        <img class='card-img-top' src='pics/pfp/pfp4.png'>
+                        <div class='card-body' style='text-align: center;'>
+                            <p>Dervisefendic Armin</p>
+                            <br>
+                            <a>Email: info@recipe-radar.at</a>
+                            <br>
+                            <a>Phone: 123-456-7890</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+
+    <?php
+        include('footer.php');
+    ?>
+
+</body>
