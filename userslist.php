ec0e94c0c29d78ecf03b5243cba4d33862128398
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-06-10 17:55:25 +0200

    Update userslist.php

--- userslist.php
+++ userslist.php
@@ -1,12 +1,15 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="res/css/design.css">
+    <link rel="stylesheet" href="resources/css/style.css">
 </head>
+
 <body>
     <?php include 'services/userslist_service.php'; ?>
     <div class="container mt-3">
@@ -19,4 +22,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
