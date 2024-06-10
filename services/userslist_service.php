044fb2ba5ed22fa6ba59d39b5696e9d8a91d958d
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-06-10 17:07:36 +0200

    userslist_service.php
    Alle users in Form von Karten anzeigen

---
+++ services/userslist_service.php
@@ -0,0 +1,39 @@
+<?php
+
+// Start session if not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Include database connection
+require 'config/db_connect.php';
+
+$sql = "SELECT * FROM users WHERE userID = {$_SESSION['currentSession']['userID']}";
+
+$result = mysqli_query($conn, $sql);
+
+$row = mysqli_fetch_assoc($result);
+
+
+
+$users = "SELECT * FROM users WHERE userTyp != 'admin'";
+$usersResult = mysqli_query($conn, $users);
+
+$layout = "";
+
+if (mysqli_num_rows($usersResult) == 0) {
+    $layout .= "No Users found!";
+} else {
+    while ($user = mysqli_fetch_assoc($usersResult)) {
+        $layout .= "<div class='card' style='width: 18rem'>
+        <div class='card-body'>
+          <h5 class='card-title'>{$user["firstName"]} {$user["lastName"]}</h5>
+          <hr>
+
+          <p class='card-text'>Username: {$user["userName"]}</p>
+          <a href='userdetails.php?id={$user["userID"]}' class='btn btn-success' >Details</a>
+        </div>
+      </div>";
+    }
+}
+?>
