e9f9d820813986b9c24924760d9ad4a266f257f1
nicholaschopper <benjamin.arapcic@hotmail.com>
2024-06-10 12:33:57 +0200

    reworked how "friends" are displayed
    Made it so that the "followers" and "followings" are separated to be more inline with our initial plan

--- services/friends_service.php
+++ services/friends_service.php
@@ -1,4 +1,6 @@
 <?php
+require 'config/db_connect.php';
+
 
 if (!isset($_SESSION['currentSession']) || !isset($_SESSION['currentSession']['userID'])) {
     header("location: ../index.php");
@@ -7,28 +9,45 @@ if (!isset($_SESSION['currentSession']) || !isset($_SESSION['currentSession']['u
 
 $currentUserID = $_SESSION['currentSession']['userID'];
 
-// Query to find friends of the current user
-$sql = "
+// Query to find users the current user is following
+$sqlFollowing = "
     SELECT 
-        CASE
-            WHEN relationFrom = '$currentUserID' THEN relationTo
-            WHEN relationTo = '$currentUserID' THEN relationFrom
-        END AS friendID
+        relationTo AS followingID
     FROM relations
-    WHERE relationFrom = '$currentUserID' OR relationTo = '$currentUserID'
+    WHERE relationFrom = '$currentUserID'
 ";
 
-$result = $conn->query($sql);
+$resultFollowing = $conn->query($sqlFollowing);
 
-if (!$result) {
+if (!$resultFollowing) {
     die("SQL Error: " . $conn->error);
 }
 
-$friends = [];
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $friends[] = $row['friendID'];
+$followings = [];
+if ($resultFollowing->num_rows > 0) {
+    while ($row = $resultFollowing->fetch_assoc()) {
+        $followings[] = $row['followingID'];
     }
 }
 
+// Query to find users who are following the current user
+$sqlFollowers = "
+    SELECT 
+        relationFrom AS followerID
+    FROM relations
+    WHERE relationTo = '$currentUserID'
+";
+
+$resultFollowers = $conn->query($sqlFollowers);
+
+if (!$resultFollowers) {
+    die("SQL Error: " . $conn->error);
+}
+
+$followers = [];
+if ($resultFollowers->num_rows > 0) {
+    while ($row = $resultFollowers->fetch_assoc()) {
+        $followers[] = $row['followerID'];
+    }
+}
 ?>
