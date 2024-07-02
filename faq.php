b424b66fbd1b92aa9bf6533336f8516b982b572c
Armin Dervisefendic <armindervisefendic@gmail.com>
2024-07-02 12:40:00 +0200

    Update faq.php

--- faq.php
+++ faq.php
@@ -31,7 +31,7 @@ include('components/head.php');
     <main>
 
         <div class="profile-row" style="max-width: 1400px; margin: 120px auto;">
-            <div class="container" style="width: 100%; height: 500px; margin: 100px auto;">
+            <div class="container" style="width: 100%; height: 900px; margin: 100px auto;">
                 <div class="faq-card">
                     <h2 class="text-center">FAQs</h2>
                     <hr>
@@ -51,6 +51,36 @@ include('components/head.php');
                     </div>
                     <div id="answer2" class="faq-answer">
                         For more questions you can contact us by email: info@recipe-radar.at
+                    </div>
+                </div>
+
+                <div class="faq-card">
+                    <hr>
+                    <div class="faq-question" onclick="toggleAnswer('answer3')">
+                        How do I post a recipe on RecipeRadar?
+                    </div>
+                    <div id="answer3" class="faq-answer">
+                        To post a recipe, visit RecipeRadar's homepage, click "Toggle Post Form," fill out your recipe details, and click "Submit."
+                    </div>
+                </div>
+
+                <div class="faq-card">
+                    <hr>
+                    <div class="faq-question" onclick="toggleAnswer('answer4')">
+                        Can I edit or delete my posted recipe?
+                    </div>
+                    <div id="answer4" class="faq-answer">
+                        Yes, you can delete your recipe by opening your post.
+                    </div>
+                </div>
+
+                <div class="faq-card">
+                    <hr>
+                    <div class="faq-question" onclick="toggleAnswer('answer5')">
+                        What should I do after submitting my post? 
+                    </div>
+                    <div id="answer5" class="faq-answer">
+                        After submitting, your recipe will be visible for others to like, comment, and share on RecipeRadar.
                     </div>
                 </div>
             </div>
